257b229a259c912704deef38f7ee223cbe0e5875
danielberco <59802615+danielberco@users.noreply.github.com>
2021-04-07 16:57:52 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,16 +1,16 @@
 #include "Board.hpp"
-#include <iostream>
 using namespace std;
+#include <iostream>
 
 namespace ariel {
-    void Board::post(unsigned int row, unsigned int column , Direction direction , string message) {
+    void Board::post(unsigned int row, unsigned int column , Direction direction , string const &message) {
         this->col_max = max(this->col_max,column);
         this->row_max = max(this->row_max,row);
         this->col_min = min(this->col_min,column);
         this->row_min = min(this->row_min,row);
 
         for (char mes: message) {
-            this->board[row][column].val = mes;
+            this->board[row][column].n = mes;
             (direction == Direction::Horizontal)? column++ : row++;
         }
     }
@@ -18,7 +18,7 @@ namespace ariel {
     string Board::read(unsigned int row, unsigned int column, Direction direction,unsigned int length) {
         string read;
         for (unsigned int i =0 ; i<length;i++) {
-            read += this->board[row][column].val;
+            read += this->board[row][column].n;
             (direction == Direction::Horizontal)? column++ : row++;
         }
         return read;
@@ -27,7 +27,7 @@ namespace ariel {
     void Board::show() {
         for (unsigned int i = this->row_min;i <= this->row_max;i++) {
             for (unsigned int j = this->col_min;j <= this->col_max;j++) {
-                cout << this->board[i][j].val << " ";
+                cout << this->board[i][j].n << " ";//space for each message
             }
             cout << "\n\n";
         }
